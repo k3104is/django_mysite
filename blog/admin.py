ff39e4a46cade8f97a119d992e6a993bd0f31eaa
Satoshi Kataoka <k.3104is@gmail.com>
2019-05-27 22:33:17 +0900

    add comment to article

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from .models import Post, Comment #Comment追加
 
 # Register your models here.
 from .models import Post
 
 admin.site.register(Post)
+admin.site.register(Comment) #この行追加
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from .models import Post, Comment #Comment追加
 
 # Create your views here.
+from django.shortcuts import render
+from . import forms #この行追加
 from django.utils import timezone
 from .models import Post
 
@@ -13,8 +15,21 @@ def post_list(request):
 # 記事専用の子ページ
 def article(request, pk):
     article = Post.objects.get(id=pk)
+    comments = Comment.objects.filter(post=article)
+
+    if request.method == "POST": #入力フォームはPOSTなので
+        form = forms.CommentForm(request.POST)
+        if form.is_valid(): #もし、formの内容が正しい時は
+            comment = form.save(commit=False) #formの内容はまだセーブしません！
+            comment.post = article
+            comment.author = request.user
+            comment.save() #ユーザーを追加したのちにセーブ
+    else:
+        form = forms.CommentForm()
 
     print(article)
     return render(request, 'blog/article.html', {
-        'article': article
-    })
+        'article': article,
+        'form': form,
+        'comments': comments
+    }) #form と comment　を追加
