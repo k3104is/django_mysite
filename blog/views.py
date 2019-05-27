da96759dd1d5e68d7b42960e236eb2b9ceed104e
“sa-kataoka” <sa-kataoka@inet1.aichitokei.co.jp>
2019-05-27 15:48:51 +0900

    make first page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+        'posts': posts
+    })
